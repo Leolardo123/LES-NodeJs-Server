0692f258a35e18a89fe2d92bf0b2d5a2bbbc2517
unknown <leoferstos5@gmail.com>
2021-08-17 21:29:31 -0300

    banco de dados básico

--- database/create.sql
+++ database/create.sql
@@ -4,23 +4,23 @@ create table elivros.clientes (
     cli_id serial primary key,
     cli_pnome varchar(255) not null,
     cli_unome varchar(255) not null,
-    cli_rg varchar(10) not null,
-    cli_cpf varchar(12) not null,
-    cli_email varchar(255) not null,
-    cli_telefone varchar(35) not null,
-    cli_senha varchar() not null
-)
+    cli_rg varchar(10) not null unique,
+    cli_cpf varchar(12) not null unique,
+    cli_email varchar(255) not null unique,
+    cli_telefone varchar(35) not null unique,
+    cli_senha text not null
+);
 
 create table elivros.livros (
     liv_id serial primary key ,
-    liv_valor num(10) not null,
+    liv_valor numeric(10,2) not null,
     liv_nome varchar(255) not null,
     liv_descricao varchar(255) not null
-)
+);
 
 create table elivros.pedidos (
     ped_id serial primary key ,
     ped_cli_id integer references elivros.clientes,
     ped_liv_id integer references elivros.livros,
     ped_data timestamp default now()
-)
+);
