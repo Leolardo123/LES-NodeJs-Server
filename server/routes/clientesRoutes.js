5f1edcf43f1ba82519fa759f3567fde145fd2da9
unknown <leoferstos5@gmail.com>
2021-08-17 21:07:51 -0300

    test

---
+++ server/routes/clientesRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/clientes',async function(req,res){
+
+})
+router.get('/clientes/:id',async function(req,res){
+
+})
+router.post('/clientes/:id',async function(req,res){
+
+})
+router.put('/clientes/:id',async function(req,res){
+
+})
+router.delete('/clientes/:id',async function(req,res){
+
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require('./config/express')();
 const port = app.get('port');
 
+app.use('/',require('./routes/clientesRoutes'))
+
 app.listen(port, () => {
   console.log(`Servidor ligado na porta ${port}`)
 });
